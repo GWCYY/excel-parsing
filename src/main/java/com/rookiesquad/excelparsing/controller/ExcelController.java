19f33a09aed4f20c57d239b4b085577fb640b546
wade <15155065624@163.com>
2023-06-30 09:46:13 +0800

    fix: .

--- src/main/java/com/rookiesquad/excelparsing/controller/ExcelController.java
+++ src/main/java/com/rookiesquad/excelparsing/controller/ExcelController.java
@@ -1,13 +1,17 @@
 package com.rookiesquad.excelparsing.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExcelController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ExcelController.class);
+
     @GetMapping("/test")
     public void testRead(String filePath){
-        System.out.println(filePath);
+        logger.info(filePath);
     }
 }
